890e949ab1bd87920a3c1a0b35cef57ded13e980
Vedema <40235234+Vedema@users.noreply.github.com>
2019-03-13 06:52:50 +0000

    Initial commoit

---
+++ blog/script.js
@@ -0,0 +1,10 @@
+$("button").on("click", function() {
+  alert("Thank you for linking this!");
+}); 
+
+//$("button") selects the element whose event we want to listen to. Also we can do $("h1") or $("p").
+// the on() function sets up an event listener for the button element.
+//"click" this is the first parameter is the type of event to listen to. This could be hover, scroll etc.
+// function() contains the thing to be done when the event 'click' happens.
+//alert("clicked!") when this event happens, it pops up this to show that a button was clicked.
+
